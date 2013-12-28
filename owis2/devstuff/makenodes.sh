21d3d5f19c338f04dcb2c0f70345baa3ffbcf750
christian.kurtsiefer@gmail.com <christian.kurtsiefer@gmail.com@76892210-332e-11de-9b0f-33ff132f608d>
2013-12-28 02:54:16 +0000

    updated loader scripts to work with opensuse 13.1

---
+++ owis2/devstuff/makenodes.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# script to generate device nodesfor PCI card drivers (dirty hack...)
+
+dev1="stepper3"
+dev1old="stepper3"
+
+# make ioboards directory
+mkdir -p /dev/ioboards
+
+# remove stale device files
+rm -f /dev/ioboards/stepper*
+rm -f /dev/stepper3*
+
+major=$(awk "\$2~\"sm32_kmodule\"  {print \$1}" /proc/devices)
+echo "sm32 driver claims following major devices:"
+echo $major
+cardindex=0 
+for elm in $major
+  do echo "creating device ${dev1}_$cardindex for major $elm"
+# create  file entries
+  mknod /dev/${dev1}_${cardindex} c $elm 1
+  chgrp users /dev/${dev1}_${cardindex}
+  chmod 664 /dev/${dev1}_${cardindex}
+  ln -s /dev/${dev1}_${cardindex} /dev/ioboards/
+              
+  cardindex=$(($cardindex+1))
+done
+
+# legacy link
+ln -s /dev/${dev1}_0 /dev/ioboards/${dev1old}
