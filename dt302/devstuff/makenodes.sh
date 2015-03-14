8d66586d26b33be670ad1e771cc9ffc2483fcd07
christian.kurtsiefer@gmail.com <christian.kurtsiefer@gmail.com@76892210-332e-11de-9b0f-33ff132f608d>
2015-03-14 03:09:42 +0000

    another fix for the udev files and makenode shell for the dt302 card

--- dt302/devstuff/makenodes.sh
+++ dt302/devstuff/makenodes.sh
@@ -17,15 +17,16 @@ major=$(awk "\$2~\"dt302\"  {print \$1}" /proc/devices)
 # echo "dt302 driver claims following major devices:" >>$lgf
 # echo $major >>$lgf
 cardindex=0 
-for elm in $major
-  do echo "creating device ${dev1}_$cardindex for major $elm"
+for elm in $major 
+do 
+#    echo "creating device ${dev1}_$cardindex for major $elm">>$lgf
 # create  file entries
-  mknod /dev/${dev1}_${cardindex} c $elm 1
-  chgrp users /dev/${dev1}_${cardindex}
-  chmod 664 /dev/${dev1}_${cardindex}
-  ln -s /dev/${dev1}_${cardindex} /dev/ioboards/
-              
-  cardindex=$(($cardindex+1))
+    mknod /dev/${dev1}_${cardindex} c $elm 1
+    chgrp users /dev/${dev1}_${cardindex}
+    chmod 664 /dev/${dev1}_${cardindex}
+    ln -s /dev/${dev1}_${cardindex} /dev/ioboards/
+    
+    cardindex=$(($cardindex+1))
 done
 
 # legacy link
