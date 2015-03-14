4a1184c8504831d0bf8297c985d59359fccf2f8b
christian.kurtsiefer@gmail.com <christian.kurtsiefer@gmail.com@76892210-332e-11de-9b0f-33ff132f608d>
2015-03-14 03:12:30 +0000

    removed echo statements in the dt340 makenode code

--- dt340/devstuff/makenodes.sh
+++ dt340/devstuff/makenodes.sh
@@ -12,18 +12,19 @@ rm -f /dev/ioboards/dt340*
 rm -f /dev/dt340*
 
 major=$(awk "\$2~\"dt340\"  {print \$1}" /proc/devices)
-echo "dt340 driver claims following major devices:"
-echo $major
+# echo "dt340 driver claims following major devices:"
+# echo $major
 cardindex=0 
 for elm in $major
-  do echo "creating device ${dev1}_$cardindex for major $elm"
-# create  file entries
-  mknod /dev/${dev1}_${cardindex} c $elm 1
-  chgrp users /dev/${dev1}_${cardindex}
-  chmod 664 /dev/${dev1}_${cardindex}
-  ln -s /dev/${dev1}_${cardindex} /dev/ioboards/
-              
-  cardindex=$(($cardindex+1))
+  do 
+    # echo "creating device ${dev1}_$cardindex for major $elm"
+    # create  file entries
+    mknod /dev/${dev1}_${cardindex} c $elm 1
+    chgrp users /dev/${dev1}_${cardindex}
+    chmod 664 /dev/${dev1}_${cardindex}
+    ln -s /dev/${dev1}_${cardindex} /dev/ioboards/
+    
+    cardindex=$(($cardindex+1))
 done
 
 # legacy link
